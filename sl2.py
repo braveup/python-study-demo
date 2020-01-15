ad1198b8c916cb4f26720d44b7e241d66f0a6324
磊 宋 <lei.song@shuyun.com>
2020-01-15 15:51:42 +0800

    列表

--- sl2.py
+++ sl2.py
@@ -13,7 +13,20 @@ full_name=first_name+last_name
 print(first_name+","+last_name+" "+full_name.title())
 
 print("hellow\nword\n\tyou!")
-name1=" s,l"
+name1=" sl "
 print(name1)
 print(name1.rstrip())
+print(name1.lstrip())
 print(name1.split())
+
+
+print(3*6)
+
+#
+print("one is "+str(1))
+
+#列表
+word=['a','b','c','d','e','f','g']
+print(word)
+print(word[0])
+print(word[0].title())
